969e7e506980061bec08fc250dcb462bef3c2003
ניסן מקסימוב <nisanmax55n@gmail.com>
2026-08-31 08:58:22 +0300

    feat: register Mishnah models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,6 +2,7 @@ from .library import Work, TextVersion, TextSegment
 from .torah import TorahBook, TorahParasha, TorahVerse
 from .neviim import NeviimBook, NeviimVerse
 from .ketuvim import KetuvimBook, KetuvimVerse
+from .mishnah import MishnahSeder, MishnahTractate, MishnahUnit
 
 __all__ = [
     "Work",
@@ -14,4 +15,7 @@ __all__ = [
     "NeviimVerse",
     "KetuvimBook",
     "KetuvimVerse",
+    "MishnahSeder",
+    "MishnahTractate",
+    "MishnahUnit",
 ]
